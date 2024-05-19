671ef5cc77d22bef45d9028a340f6b302ae6f56a
cfl.dev.apps <cfl.dev.apps@gmail.com>
2024-05-19 09:43:08 +0100

    updating index page para testar ambiente de homologação

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
     <div>
       <h1>Pagina em consturção</h1>
       <h4>Pagina sendo utlizada para teste do Dominio cfl-dev-apps.com.br</h4>
+      <h5>Branch de homologacao</h5>
     </div>
   );
 }
